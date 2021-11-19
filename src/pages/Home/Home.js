a6bdab52dae95fc186b6803e61b9c6fa89782162
km-ji <90051822+mjikim@users.noreply.github.com>
2021-11-20 02:48:08 +0900

    client #1

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -42,7 +42,7 @@ const Home = () => {
     }
 
     return (
-        <> 
+        <>
             <Header/>
             <IssueRanking/>
             <div className="home-category-wrap">
@@ -67,35 +67,35 @@ const Home = () => {
                         />
                     </NavLink>
                 )}
-            {/* </div>
+            </div>
         </>
-                <div className="background">
-                <div className="menubar">
-                    <div className="issueUp">
-                    <ul>
-                    <li className="txtUp"><a href="#">이슈업!로드</a></li>
-                    </ul>
-                    </div>
-                    <div className="main">
-                        THISSUE
-                    </div>
+            //     <div className="background">
+            //     <div className="menubar">
+            //         <div className="issueUp">
+            //         <ul>
+            //         <li className="txtUp"><a href="#">이슈업!로드</a></li>
+            //         </ul>
+            //         </div>
+            //         <div className="main">
+            //             THISSUE
+            //         </div>
     
-                    <div> 
-                        <input type="text" className="input"></input>
-                        <img scr="./search.png" className="search"></img>
-                    </div>
+            //         <div> 
+            //             <input type="text" className="input"></input>
+            //             <img scr="./search.png" className="search"></img>
+            //         </div>
     
-                    <div className="login">
-                        <ul>
-                        <li className="txtLog"><a href="#">Login</a></li>
-                        </ul>
-                    </div>
-                </div>
+            //         <div className="login">
+            //             <ul>
+            //             <li className="txtLog"><a href="#">Login</a></li>
+            //             </ul>
+            //         </div>
+            //     </div>
     
-                <div className="txtRanking">
-                    실시간 이슈 랭킹🔥
-                </div>
-            </div> */}
+            //     <div className="txtRanking">
+            //         실시간 이슈 랭킹🔥
+            //     </div>
+            // </div>
 
 
 
