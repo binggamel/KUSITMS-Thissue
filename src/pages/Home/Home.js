1aea1b40ca6a0b5001b118b4657c7c8a211fa3f5
km-ji <90051822+mjikim@users.noreply.github.com>
2021-11-20 12:59:42 +0900

    client #1

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -69,35 +69,6 @@ const Home = () => {
                 )}
             </div>
         </>
-            //     <div className="background">
-            //     <div className="menubar">
-            //         <div className="issueUp">
-            //         <ul>
-            //         <li className="txtUp"><a href="#">이슈업!로드</a></li>
-            //         </ul>
-            //         </div>
-            //         <div className="main">
-            //             THISSUE
-            //         </div>
-    
-            //         <div> 
-            //             <input type="text" className="input"></input>
-            //             <img scr="./search.png" className="search"></img>
-            //         </div>
-    
-            //         <div className="login">
-            //             <ul>
-            //             <li className="txtLog"><a href="#">Login</a></li>
-            //             </ul>
-            //         </div>
-            //     </div>
-    
-            //     <div className="txtRanking">
-            //         실시간 이슈 랭킹🔥
-            //     </div>
-            // </div>
-
-
 
     )
 }
