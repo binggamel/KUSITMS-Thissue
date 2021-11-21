bec0cbcfe12c173d154693c9b18167fe3d9b7cda
binggamel <kcyeun1203@naver.com>
2021-11-21 21:41:35 +0900

    fix err at  mongoose.connect

--- server/server.js
+++ server/server.js
@@ -10,13 +10,11 @@ const { auth } = require("./middleware/auth");
 const { User } = require("./model/User");
 const config = require("./config/key");
 
-//config.mongoURI
-// process.env.MONGO_URL
+//process.env.MONGO_URL
+//MONGO_URL=mongodb://localhost/thissue
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  //   useCreateIndex: true,
-  //   useFindAndModify: false,
 });
 
 var db = mongoose.connection;
@@ -30,16 +28,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, "public")));
 // app.use("/up", require("./router/up"));
-
-//Authentication(미들웨어를 통과함 = Authentication이 true. 인증 통과~!)
-// app.get("/auth", auth, (req, res) => {
-//   res.status(200).json({
-//     _id: req.user._id,
-//     isAuth: true,
-//     email: req.user.email,
-//     name: req.user.name,
-//   });
-// });
 
 app.get("/", (req, res) => res.send("핫식스 아좌아좌빠이띵~"));
 
